4c816a977107500f2f2c4ad473ac399440055a0d
MSinghSolanki <112701441+MSinghSolanki@users.noreply.github.com>
2023-06-02 14:57:37 +0530

    new

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,21 +1,19 @@
-
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
-import {BsSearch} from "react-icons/bs";
-import {Weather} from "./components/Weather"
+import { BsSearch } from 'react-icons/bs';
+import { Weather } from './components/Weather';
 import { Spinner } from './components/Spinner';
-import logo1 from "./videos/bg.mp4"
-import logo2 from "./videos/Clouds.mp4"
-import logo3 from "./videos/Clear.mp4"
-import logo4 from "./videos/rain.mp4"
-import logo5 from "./videos/thunder.mp4"
-import logo6 from "./videos/Snow.mp4"
-import logo7 from "./videos/Smoke.mp4"
-import logo8 from "./videos/Drizzle.mp4"
+import logo1 from './videos/bg.mp4';
+import logo2 from './videos/Clouds.mp4';
+import logo3 from './videos/Clear.mp4';
+import logo4 from './videos/rain.mp4';
+import logo5 from './videos/thunder.mp4';
+import logo6 from './videos/Snow.mp4';
+import logo7 from './videos/Smoke.mp4';
+import logo8 from './videos/Drizzle.mp4';
 
 function App() {
-
   const videoSources = {
     Thunderstorm: logo5,
     Drizzle: logo8,
@@ -23,57 +21,53 @@ function App() {
     Snow: logo6,
     Clear: logo3,
     Clouds: logo2,
-    Smoke:logo7,
+    Smoke: logo7,
   };
-const[loading,setLoading] = useState(false);
-const[city,setCity] = useState("");
-const [weather,setWeather]= useState({});
-const url = (`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=360e96144e8eb0fca116c4be6816e37e`)
-const FetchWeather = (event)=>{
-  event.preventDefault()
-setLoading(true)
-axios.get(url).then((res)=>{
-  setWeather(res.data)
- 
-  })
-  setLoading(false)
-  setCity('')
- 
-}
-if(loading){
-return <Spinner/>
-}
-else{
 
+  const [loading, setLoading] = useState(false);
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState({});
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=360e96144e8eb0fca116c4be6816e37e`;
 
-  return (
-    <div>
+  const fetchWeather = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    axios.get(url).then((res) => {
+      setWeather(res.data);
+      setLoading(false);
+    });
+    setCity('');
+  };
+
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
       <div>
-      {weather.main && (
-            <video
-              src={videoSources[weather.weather[0].main]}
-              autoPlay
-              muted
-              loop
-            />
+        <div>
+          {Object.keys(weather).length === 0 && weather.constructor === Object ? (
+            <video src={logo1} autoPlay muted loop />
+          ) : (
+            <video src={videoSources[weather.weather[0].main]} autoPlay muted loop />
           )}
         </div>
-<div className='search'>
-  <form onSubmit={FetchWeather} className='search1'>
-<div>
-  <div>
-    <input onChange={(e)=>setCity(e.target.value)} type='text' placeholder='Search city'/>
-    </div>
-    </div>
-    <button onClick={FetchWeather}><BsSearch size={20}/></button>
-  </form>
-</div>
+        <div className="search">
+          <form onSubmit={fetchWeather} className="search1">
+            <div>
+              <div>
+                <input onChange={(e) => setCity(e.target.value)} type="text" placeholder="Search city" />
+              </div>
+            </div>
+            <button onClick={fetchWeather}>
+              <BsSearch size={20} />
+            </button>
+          </form>
+        </div>
 
-{weather.main &&<Weather data={weather}/>}
-
-     </div>
-  );
-}
+        {Object.keys(weather).length > 0 && weather.constructor === Object && <Weather data={weather} />}
+      </div>
+    );
+  }
 }
 
 export default App;
